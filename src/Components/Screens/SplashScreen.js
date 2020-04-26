e1129e6d547773523c268fe924d80e2737f8ba6c
Azeem Tariq <azeemtariq00@gmail.com>
2020-04-26 21:04:11 +0500

    Push notifications

--- src/Components/Screens/SplashScreen.js
+++ src/Components/Screens/SplashScreen.js
@@ -1,16 +1,24 @@
 import React from 'react';
-
 import { connect } from "react-redux";
+import { StyleSheet, View, Button, Vibration, Platform } from 'react-native';
+import { Notifications } from 'expo';
+import * as Permissions from 'expo-permissions';
+import Constants from 'expo-constants';
 
-import { StyleSheet, View, Image } from 'react-native';
 import * as NavigationService from '../../NavigationService'
 import * as actions from '../../Store/Actions/AuthActions';
 
 class SplashScreen extends React.Component {
 
+  state = {
+    expoPushToken: '',
+    notification: {},
+  };
 
-  componentDidMount() {
+  async componentDidMount() {
     const { syncWithAsyncStorage } = this.props;
+    await this.registerForPushNotificationsAsync();
+    this._notificationSubscription = Notifications.addListener(this._handleNotification);
     syncWithAsyncStorage({
       onSuccess: ({user, skiped}) => {
         if(user !== undefined){
@@ -20,6 +28,42 @@ class SplashScreen extends React.Component {
       }
     });
   }
+
+  
+  _handleNotification = notification => {
+    Vibration.vibrate();
+    console.log(notification);
+    this.setState({ notification: notification });
+  };
+  
+  registerForPushNotificationsAsync = async () => {
+    if (Constants.isDevice) {
+      const { status: existingStatus } = await Permissions.getAsync(Permissions.NOTIFICATIONS);
+      let finalStatus = existingStatus;
+      if (existingStatus !== 'granted') {
+        const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+        finalStatus = status;
+      }
+      if (finalStatus !== 'granted') {
+        alert('Failed to get push token for push notification!');
+        return;
+      }
+      token = await Notifications.getExpoPushTokenAsync();
+      console.log({token});
+      this.setState({ expoPushToken: token });
+    } else {
+      alert('Must use physical device for Push Notifications');
+    }
+
+    if (Platform.OS === 'android') {
+      Notifications.createChannelAndroidAsync('default', {
+        name: 'default',
+        sound: true,
+        priority: 'max',
+        vibrate: [0, 250, 250, 250],
+      });
+    }
+  };
 
   render() {
     
